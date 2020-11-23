c1c478e10e1de04d70810be7b966a4e2f0536517
Dan O'Connor <danocon@microsoft.com>
2020-11-23 11:34:34 -0500

    Include user ID in returned token

--- WebService/FitWithFriends/routes/auth.js
+++ WebService/FitWithFriends/routes/auth.js
@@ -5,40 +5,13 @@ const oauthServer = require('../oauth/server.js')
 
 const router = express.Router() // Instantiate a new router
 
-router.get('/', (req, res) => {  // send back a simple form for the oauth
-    res.render('login', { title: 'Login' })
-})
-
-
-router.post('/authorize', (req, res, next) => {
-    const { username, password } = req.body
-    //if (username === 'test' && password === 'test') {
-    //    req.body.user = { user: 1 }
-    //    return next()
-    //}
-    return next();
-
-    //const params = [ // Send params back down
-    //    'client_id',
-    //    'redirect_uri',
-    //    'response_type',
-    //    'grant_type',
-    //    'state',
-    //]
-    //    .map(a => `${a}=${req.body[a]}`)
-    //    .join('&')
-    //return res.redirect(`/oauth?success=false&${params}`)
-}, (req, res, next) => { // sends us to our redirect with an authorization code in our url
-    return next()
-}, oauthServer.authorize())
-
 router.post('/token', (req, res, next) => {
     next()
 }, oauthServer.token({
-    requireClientAuthentication: { // whether client needs to provide client_secret
-        'authorization_code': true,
-    },
-}))  // Sends back token
+    // Send back extra properties that the model sets on the created token
+    // We want this so user ID is returned with the token
+    allowExtendedTokenAttributes: true
+}))
 
 
 module.exports = router
