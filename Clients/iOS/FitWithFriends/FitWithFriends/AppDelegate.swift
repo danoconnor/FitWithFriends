85ea8431c6bcc50d24002ba3635b07ff422352fe
Dan O'Connor <danocon@microsoft.com>
2020-11-29 14:40:58 -0500

    Add logic to get data from HealthKit

--- Clients/iOS/FitWithFriends/FitWithFriends/AppDelegate.swift
+++ Clients/iOS/FitWithFriends/FitWithFriends/AppDelegate.swift
@@ -12,7 +12,29 @@ class AppDelegate: NSObject { }
 
 extension AppDelegate: UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Logger.traceInfo(message: "------------------------------ didFinishLaunchingWithOptions (options: \(launchOptions?.map { $0.key }.description ?? "none")) ------------------------------")
+
+        // Apple's docs suggest that we register our queries in didFinishLaunchingWithOptions so that they will be executed
+        // properly when the app is launched in the background
+        // https://developer.apple.com/documentation/healthkit/hkobserverquery/executing_observer_queries
+        ObjectGraph.sharedInstance.healthKitManager.registerDataQueries()
+
+        // Always register for background health data updates, if available
+        ObjectGraph.sharedInstance.healthKitManager.registerForBackgroundUpdates()
+
         return true
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        Logger.traceInfo(message: "------------------------------ applicationDidBecomeActive ------------------------------")
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        Logger.traceInfo(message: "------------------------------ applicationDidEnterBackground ------------------------------")
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        Logger.traceInfo(message: "------------------------------ applicationWillTerminate ------------------------------")
     }
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
--- Clients/iOS/FitWithFriends/FitWithFriends/Data/WorkoutData/HealthKitManager.swift
+++ Clients/iOS/FitWithFriends/FitWithFriends/Data/WorkoutData/HealthKitManager.swift
@@ -13,7 +13,11 @@ class HealthKitManager {
     private let authenticationManager: AuthenticationManager
     private let userDefaults: UserDefaults
 
+    private let hkHealthStore = HKHealthStore()
+
     private static let healthPromptKey = "HasPromptedForHealthPermissions"
+    private static let lastUpdateKey = "LastHealthDataUpdate"
+    private static let lastWorkoutAnchorKey = "LastWorkoutAnchor"
 
     private var loginStateCancellable: AnyCancellable?
 
@@ -45,7 +49,7 @@ class HealthKitManager {
             .activitySummaryType()
         ]
 
-        HKHealthStore().requestAuthorization(toShare: nil, read: Set(dataTypes)) { [weak self] success, error in
+        hkHealthStore.requestAuthorization(toShare: nil, read: Set(dataTypes)) { [weak self] success, error in
             if let error = error {
                 Logger.traceError(message: "Failed to request authorization for health data", error: error)
                 completion()
@@ -56,9 +60,133 @@ class HealthKitManager {
 
             if success {
                 self?.userDefaults.set(true, forKey: HealthKitManager.healthPromptKey)
+                self?.registerForBackgroundUpdates()
             }
 
             completion()
         }
+    }
+
+    func registerForBackgroundUpdates() {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            Logger.traceWarning(message: "Health data is not available on this device, not registering for background updates")
+            return
+        }
+
+        hkHealthStore.enableBackgroundDelivery(for: .activitySummaryType(), frequency: .hourly) { success, error in
+            if let error = error {
+                Logger.traceError(message: "Failed to enable background delivery for activity summary", error: error)
+            }
+
+            Logger.traceInfo(message: "Enabled background delivery for activity summary: \(success)")
+        }
+
+        hkHealthStore.enableBackgroundDelivery(for: .workoutType(), frequency: .immediate) { success, error in
+            if let error = error {
+                Logger.traceError(message: "Failed to enable background delivery for workouts", error: error)
+            }
+
+            Logger.traceInfo(message: "Enabled background delivery for workouts: \(success)")
+        }
+    }
+
+    func registerDataQueries() {
+        registerActivitySummaryQuery()
+        registerWorkoutQuery()
+    }
+
+    private func registerActivitySummaryQuery() {
+        let dateRange = getQueryDateRange()
+        let predicate = HKQuery.predicate(forActivitySummariesBetweenStart: dateRange.startCompenents,
+                                          end: dateRange.endComponents)
+
+        Logger.traceInfo(message: "Executing activity summary query for dates \(dateRange.startCompenents.description) to \(dateRange.endComponents.description)")
+        let query = HKActivitySummaryQuery(predicate: predicate, resultsHandler: handleActivitySummaryData(query:summaries:error:))
+        query.updateHandler = handleActivitySummaryData(query:summaries:error:)
+        hkHealthStore.execute(query)
+    }
+
+    private func registerWorkoutQuery() {
+        var anchor: HKQueryAnchor?
+        if let previousAnchorData = userDefaults.data(forKey: HealthKitManager.lastWorkoutAnchorKey),
+           let unarchiver = try? NSKeyedUnarchiver(forReadingFrom: previousAnchorData),
+           let previousAnchor = unarchiver.decodeObject() as? HKQueryAnchor {
+            anchor = previousAnchor
+        }
+
+        let query = HKAnchoredObjectQuery(type: .workoutType(), predicate: nil, anchor: anchor, limit: HKObjectQueryNoLimit, resultsHandler: handleWorkoutData(query:workouts:deletedObjects:anchor:error:))
+        query.updateHandler = handleWorkoutData(query:workouts:deletedObjects:anchor:error:)
+        hkHealthStore.execute(query)
+    }
+
+    private func handleActivitySummaryData(query: HKQuery, summaries: [HKActivitySummary]?, error: Error?) {
+        if let error = error {
+            Logger.traceError(message: "Error when querying activity summary data", error: error)
+            return
+        }
+
+        Logger.traceInfo(message: "Received activity summary update with \(summaries?.count ?? 0) new summaries")
+
+        userDefaults.setValue(Date().timeIntervalSince1970, forKey: HealthKitManager.lastUpdateKey)
+        if let summaries = summaries {
+            for summary in summaries {
+                // TODO: send to service
+                print(summary.description)
+            }
+        }
+    }
+
+    private func handleWorkoutData(query: HKAnchoredObjectQuery, workouts: [HKSample]?, deletedObjects: [HKDeletedObject]?, anchor: HKQueryAnchor?, error: Error?) {
+        if let error = error {
+            Logger.traceError(message: "Error when querying workout data", error: error)
+            return
+        }
+
+        Logger.traceInfo(message: "Received workout update with \(workouts?.count ?? 0) new workouts")
+
+        if let anchor = anchor {
+            let archiver = NSKeyedArchiver(requiringSecureCoding: true)
+            archiver.encode(anchor)
+
+            userDefaults.setValue(archiver.encodedData, forKey: HealthKitManager.lastWorkoutAnchorKey)
+        }
+
+        if let workouts = workouts {
+            for workoutSample in workouts {
+                guard let workout = workoutSample as? HKWorkout else {
+                    Logger.traceWarning(message: "Sample was not a workout type, it was \(type(of: workoutSample))")
+                    continue
+                }
+
+                // TODO: send to service
+                print(workout.description)
+            }
+        }
+    }
+
+    private func getQueryDateRange() -> (startCompenents: DateComponents, endComponents: DateComponents) {
+        // Set the query end date to a month in the future so we continue to receive updates after the app has launched and is sitting in memory
+        let calendar = Calendar.current
+        let queryStartDate: Date
+        let queryEndDate = calendar.date(byAdding: .day, value: 30, to: Date())!
+
+        let lastUpdate = userDefaults.double(forKey: HealthKitManager.lastUpdateKey)
+        if lastUpdate > 0 {
+            // The last update is stored in user defaults as the timeIntervalSince1970 value
+            queryStartDate = Date(timeIntervalSince1970: lastUpdate)
+        } else {
+            // If we haven't received any health data before, default to querying the last 7 days
+            queryStartDate = calendar.date(byAdding: .day, value: -7, to: queryEndDate)!
+        }
+
+        let dateComponents: Set<Calendar.Component> = [.day, .month, .year, .era]
+
+        var startDateComponents = calendar.dateComponents(dateComponents, from: queryStartDate)
+        startDateComponents.calendar = calendar
+
+        var endDateComponents = calendar.dateComponents(dateComponents, from: queryEndDate)
+        endDateComponents.calendar = calendar
+
+        return (startDateComponents, endDateComponents)
     }
 }
