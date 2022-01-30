88719599cb216d4e7d0a4dcc22da2ba172f6c822
Dan O'Connor <dpo3ys@virginia.edu>
2022-01-30 12:03:02 -0500

    Change AT lifetime back to 1 day

--- WebService/FitWithFriends/oauth/server.js
+++ WebService/FitWithFriends/oauth/server.js
@@ -3,7 +3,7 @@ const OAuthServer = require('express-oauth-server')
 module.exports = new OAuthServer({
     model: require('./model'),
     grants: ['refresh_token', 'password'],
-    accessTokenLifetime: 60, // 60 * 60 * 24, // 1 day
+    accessTokenLifetime:  60 * 60 * 24, // 1 day
     refreshTokenLifetime: 60 * 60 * 24 * 365, // 1 year
     allowEmptyState: true,
     allowExtendedTokenAttributes: true
