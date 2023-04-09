ecb79a0359b771581f66d42c873cac52f6457ee0
Dan O'Connor <dpo3ys@virginia.edu>
2023-04-08 21:54:26 -0400

    Remove admin endpoints

--- WebService/FitWithFriends/admin/adminAuthMiddleware.js
+++
@@ -1,8 +0,0 @@
-module.exports.authenticateAdminClient = function (req, res, next) {
-    if (!req.client || !req.client.authorized) {
-        res.send(401);
-        return;
-    }
-
-    next();
-}
--- WebService/FitWithFriends/app.js
+++ WebService/FitWithFriends/app.js
@@ -6,11 +6,6 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const https = require('https');
-const fs = require('fs');
-const { DefaultAzureCredential } = require('@azure/identity');
-const { CertificateClient } = require("@azure/keyvault-certificates");
-const { SecretClient } = require('@azure/keyvault-secrets');
 
 const routes = require('./routes/index');
 const users = require('./routes/users');
@@ -20,12 +15,6 @@ const activityData = require('./routes/activityData');
 const pushNotifications = require('./routes/pushNotifications');
 const wellKnown = require('./routes/wellKnown');
 const globalConfig = require('./utilities/globalConfig')
-
-// Admin routes
-const adminAuthMiddleware = require('./admin/adminAuthMiddleware');
-const activityDataManagement = require('./admin/routes/activityDataManagement');
-const competitionManagement = require('./admin/routes/competitionManagement');
-const tokenManagement = require('./admin/routes/tokenManagement');
 
 const oauthServer = require('./oauth/server');
 const config = require('./utilities/globalConfig');
@@ -60,11 +49,6 @@ app.use('/activityData', oauthServer.authenticate(), activityData);
 app.use('/pushNotifications', oauthServer.authenticate(), pushNotifications);
 app.use('/.well-known', wellKnown);
 
-// Admin routes
-app.use('/admin/activityData', adminAuthMiddleware.authenticateAdminClient, activityDataManagement);
-app.use('/admin/competitions', adminAuthMiddleware.authenticateAdminClient, competitionManagement);
-app.use('/admin/tokens', adminAuthMiddleware.authenticateAdminClient, tokenManagement);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     const err = new Error('Not Found');
@@ -89,55 +73,4 @@ app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
-
-// Require client TLS for some requests related to admin commands
-// Because our app service in Azure will shutdown when there is no traffic,
-// we cannot rely on the app service to run our cron jobs.
-// Instead, we've created some admin endpoints that can be called by an external server
-// to handle cron job type tasks like sending push notifications, archiving completed competitions, cleaning up expired tokens, etc.
-// Any client calling these admin commands will need to authenticate with a client certificate so we can ensure that the request
-// is coming from a trusted device.
-
-// Need to get HTTPS cert/key and the CA for the client
-const vaultUrl = process.env.AZURE_KEYVAULT_URL;
-const credential = new DefaultAzureCredential();
-const certClient = new CertificateClient(vaultUrl, credential);
-certClient.getCertificate(process.env.HTTPS_CERT_NAME)
-    .then(httpsCertSecret => {
-        console.log('Cert fetch complete, fetching private key');
-
-        // Convert HTTPS cert to PEM format
-        const certBase64 = httpsCertSecret.cer.toString('base64');
-        const httpsCertPem = '-----BEGIN CERTIFICATE-----\n' +
-            certBase64.match(/.{1,64}/g).join('\n') +
-            '\n-----END CERTIFICATE-----';
-
-        // Parse the url to get the secret name and version
-        const secretNameVersionRegex = 'https:\/\/.*\/secrets\/(.*)\/(.*)\?'
-        const regexResults = httpsCertSecret.secretId.match(secretNameVersionRegex);
-        const httpsKeySecretName = regexResults[1];
-        const httpsKeySecretVersion = regexResults[2];
-
-        const secretClient = new SecretClient(vaultUrl, credential);
-        secretClient.getSecret(httpsKeySecretName, httpsKeySecretVersion)
-            .then(httpsPrivateKeySecret => {
-                console.log('HTTPS private key fetch complete');
-
-                const clientTLSOptions = {
-                    key: httpsPrivateKeySecret.value,
-                    cert: httpsCertPem,
-                    ca: fs.readFileSync('admin\\FWFClientTLSCA.pem'),
-                    requestCert: true,
-                    rejectUnauthorized: true
-                };
-                const clientTLSServer = https.createServer(clientTLSOptions, app);
-                clientTLSServer.listen(3001);
-            })
-            .catch(error => {
-                 console.error('Error fetching HTTPS private key on app launch: ', error);
-            });
-    })
-    .catch(error => {
-        console.error('Error fetching HTTPS cert on app launch: ', error);
-    });
 
