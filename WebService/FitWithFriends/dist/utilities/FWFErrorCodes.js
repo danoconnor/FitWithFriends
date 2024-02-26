8d35555a951bc46a259b90a4508b3e4f3058e719
Dan O'Connor <danocon@microsoft.com>
2024-02-26 17:18:40 -0500

    Ignore typescript dist

--- WebService/FitWithFriends/dist/utilities/FWFErrorCodes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// A list of specific error codes to return to the client
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    CompetitionErrorCodes: {
-        // The user has too many active competitions and cannot join/create a new one
-        TooManyActiveCompetitions: 10001
-    }
-};
--- WebService/FitWithFriends/dist/utilities/PushNotificationPlatform.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Maps platform names to the ints that will be stored in the database
-exports.default = {
-    iOS: 1
-};
