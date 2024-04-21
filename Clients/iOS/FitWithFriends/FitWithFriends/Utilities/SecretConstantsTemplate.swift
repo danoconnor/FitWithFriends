10991e2f9309f052d117aa53dcc0e35b54e028d0
Dan O'Connor <dpo3ys@virginia.edu>
2024-04-21 19:51:18 -0400

    Use SecretConstantsTemplate file and use bash script to fill it in

---
+++ Clients/iOS/FitWithFriends/FitWithFriends/Utilities/SecretConstantsTemplate.swift
@@ -0,0 +1,34 @@
+//
+//  SecretConstants.swift
+//  FitWithFriends
+//
+//  Created by Dan O'Connor on 11/25/23.
+//
+
+import Foundation
+
+struct SecretConstants {
+    static let keychainPrefix = "U7MJ9CBUMD"
+
+    static let supportEmail = "fitwithfriendsapp@gmail.com"
+
+    static let baseAppProtocol = "fitwithfriends"
+
+    static let developerName = "Dan O'Connor"
+
+    // MARK: Local testing values
+
+    static let localServiceBaseUrl = "https://localhost:3000"
+
+    static let localClientSecret = "11279ed4-2687-408d-9ae7-22ab3ca41219"
+
+    static let localClientId = "6a773c32-5eb3-41c9-8036-b991b51f14f7"
+
+    // MARK: Production values
+
+    static let prodServiceBaseUrl = "TODO"
+
+    static let prodClientSecret = "TODO"
+
+    static let prodClientId = "TODO"
+}
