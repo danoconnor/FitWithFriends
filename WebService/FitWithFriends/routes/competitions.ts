93e9e08a389fb66885d7d24cb694cff1a4b43ea9
Dan O'Connor <3710343+danoconnor@users.noreply.github.com>
2026-03-27 17:58:41 -0400

    Fix tests

--- WebService/FitWithFriends/routes/competitions.ts
+++ WebService/FitWithFriends/routes/competitions.ts
@@ -254,7 +254,7 @@ router.get('/:competitionId/overview', function (req, res) {
                         'competitionState': competitionInfo.state,
                         'isCompetitionProcessingResults': competitionInfo.state === CompetitionState.ProcessingResults,
                         'isUserAdmin': isUserAdmin,
-                        'currentResults': userPoints
+                        'currentResults': Object.values(userPoints)
                     });
                 })
                 .catch(error => {
